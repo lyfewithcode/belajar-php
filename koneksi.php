36c3d06cac8b1bccb511ff3ee73253d3b65af1f0
lyfewithcode <lyfewithcode@gmail.com>
2020-10-24 22:36:35 +0700

    Create new table using php in mysql

--- koneksi.php
+++ koneksi.php
@@ -2,8 +2,8 @@
     $host = "localhost"; //host database
     $username = "root";  //username database
     $password = ""; //password database
-
-    $koneksi = new mysqli($host, $username, $password); //membuat koneksi ke server mysql
+    $db = "profile";
+    $koneksi = new mysqli($host, $username, $password, $db); //membuat koneksi ke server mysql
 
     if ($koneksi->connect_error){
         die("Koneksi ke database gagal");
